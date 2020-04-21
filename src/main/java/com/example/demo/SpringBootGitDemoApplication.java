36557b22e06923685fb0c6d67dde64e91ccf28e0
Sunil Kumar Jakhal <you@example.com>
2020-04-21 12:33:27 +0530

    add sop

--- src/main/java/com/example/demo/SpringBootGitDemoApplication.java
+++ src/main/java/com/example/demo/SpringBootGitDemoApplication.java
@@ -8,6 +8,7 @@ public class SpringBootGitDemoApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(SpringBootGitDemoApplication.class, args);
+		System.out.println("Hello");
 	}
 
 }
